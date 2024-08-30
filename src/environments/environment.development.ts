88053b8f5a901805b547da20e4b2086c201a53a4
bacallaV <valdohalo3@hotmail.com>
2024-08-30 09:25:13 -0600

    feat: :triangular_flag_on_post: add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+import IEnvironment from "./environment.interface";
+
+export const environment: IEnvironment = {
+  API_URL: 'http://localhost:5432/api',
+};
---
+++ src/environments/environment.interface.ts
@@ -0,0 +1,5 @@
+interface _IEnvironment {
+  API_URL: string;
+}
+
+export default interface IEnvironment extends Readonly<_IEnvironment> {}

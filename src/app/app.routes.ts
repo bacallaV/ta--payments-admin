cb9ed07fe2ee5b44a412e78e55838956fc02b4aa
bacallaV <valdohalo3@hotmail.com>
2024-08-30 09:46:31 -0600

    feat: :building_construction: create base project architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,51 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Layouts
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+// Pages
+import {
+  HomePageComponent,
+  LoginPageComponent,
+  UnknownPageComponent,
+  CustomerPageComponent,
+  PaymentsPageComponent,
+} from './pages';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+      },
+      {
+        path: 'home',
+        component: HomePageComponent,
+      },
+      {
+        path: 'customers',
+        component: CustomerPageComponent,
+      },
+      {
+        path: 'payments',
+        component: PaymentsPageComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: UnknownPageComponent,
+  },
+];
---
+++ src/app/pages/index.ts
@@ -0,0 +1,5 @@
+export * from './home-page/home-page.component';
+export * from './login-page/login-page.component';
+export * from './unknown-page/unknown-page.component';
+export * from './customer-page/customer-page.component';
+export * from './payments-page/payments-page.component';

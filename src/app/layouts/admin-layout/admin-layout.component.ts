a333e871f904f8e9920c95480f4d5b4cc09dedec
bacallaV <valdohalo3@hotmail.com>
2024-08-31 00:24:56 -0600

    feat: :triangular_flag_on_post: add admin layout

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    CommonModule,
+  ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
